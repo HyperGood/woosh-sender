80f0eaed44b438d6186e51698c6fdbb9cc2c0569
roy-sandoval <roy@roysandoval.com>
2023-07-04 19:09:59 -0700

    Add connectkit

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,19 @@ await import("./src/env.mjs");
 /** @type {import("next").NextConfig} */
 const config = {
   reactStrictMode: true,
+  future: {
+    webpack5: true,
+  },
+  webpack(config) {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+
+    return config;
+  },
 
   /**
    * If you have `experimental: { appDir: true }` set, then you must comment the below `i18n` config
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,36 @@
 import { type AppType } from "next/app";
 import { api } from "~/utils/api";
 import "~/styles/globals.css";
+import { WagmiConfig, createConfig } from "wagmi";
+import { ConnectKitProvider, getDefaultConfig } from "connectkit";
+import { optimismGoerli } from "wagmi/chains";
+const chains = [optimismGoerli];
+
+const config = createConfig(
+  getDefaultConfig({
+    // Required API Keys
+    chains,
+    alchemyId: process.env.ALCHEMY_ID, // or infuraId
+    walletConnectProjectId: process.env.WALLETCONNECT_PROJECT_ID || "",
+
+    // Required
+    appName: "Your App Name",
+
+    // Optional
+    appDescription: "Your App Description",
+    appUrl: "https://family.co", // your app's url
+    appIcon: "https://family.co/logo.png", // your app's icon, no bigger than 1024x1024px (max. 1MB)
+  })
+);
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <WagmiConfig config={config}>
+      <ConnectKitProvider>
+        <Component {...pageProps} />
+      </ConnectKitProvider>
+    </WagmiConfig>
+  );
 };
 
 export default api.withTRPC(MyApp);
