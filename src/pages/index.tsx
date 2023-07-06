e02dbe0067043028f6df9190ba0b9f8de2a655a6
roy-sandoval <roy@roysandoval.com>
2023-07-06 00:14:12 -0700

    Add mock transactions to database and display

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,98 @@
 import type { Transaction } from "@prisma/client";
 import { useSession } from "next-auth/react";
-import { useState } from "react";
+import { useReducer } from "react";
 import { useAccount } from "wagmi";
 import Layout from "~/components/layout";
 import { api } from "~/utils/api";
+
+interface TransactionForm {
+  amount: number;
+  token: string;
+  phone: string;
+  recipient?: string;
+}
+
+const Send = () => {
+  const [transaction, setFields] = useReducer(
+    (
+      current: TransactionForm,
+      update: Partial<TransactionForm>
+    ): TransactionForm => ({
+      ...current,
+      ...update,
+    }),
+    {
+      amount: 0,
+      token: "",
+      phone: "",
+    }
+  );
+
+  const ctx = api.useContext();
+
+  const { mutate, isLoading: isSending } = api.transaction.add.useMutation({
+    onSuccess: () => {
+      console.log("Sent!");
+      void ctx.transaction.getTransactions.invalidate();
+    },
+  });
+
+  const sendFunction = async () => {
+    if (transaction.amount !== 0 && transaction.phone !== "") {
+      const amountInUSD = transaction.amount * 2000;
+      const txId = "0x1234";
+      mutate({
+        ...transaction,
+        amountInUSD: amountInUSD,
+        txId: txId,
+      });
+    }
+  };
+
+  return (
+    <div className="flex gap-8">
+      <input
+        type="number"
+        value={transaction.amount}
+        onChange={(e) => setFields({ amount: parseInt(e.target.value) })}
+        className="border-2 border-gray-300 p-2"
+        placeholder="Amount"
+        min={0}
+        required
+      />
+      <input
+        type="text"
+        value={transaction.token}
+        onChange={(e) => setFields({ token: e.target.value })}
+        className="border-2 border-gray-300 p-2"
+        placeholder="ETH"
+        required
+      />
+      <input
+        type="text"
+        value={transaction.phone}
+        onChange={(e) => setFields({ phone: e.target.value })}
+        className="border-2 border-gray-300 p-2"
+        placeholder="Phone number"
+        required
+      />
+      <input
+        type="text"
+        value={transaction.recipient}
+        onChange={(e) => setFields({ recipient: e.target.value })}
+        className="border-2 border-gray-300 p-2"
+        placeholder="Recipient Name"
+      />
+      <button
+        onClick={() => void sendFunction()}
+        disabled={isSending}
+        className="rounded-full bg-gray-100 px-12 py-4 transition-colors hover:bg-gray-300 disabled:cursor-not-allowed disabled:opacity-60"
+      >
+        Send
+      </button>
+    </div>
+  );
+};
 
 const TransactionCard = ({ transaction }: { transaction: Transaction }) => {
   return (
@@ -44,16 +133,17 @@ export default function Home() {
   // const [newTransaction, setNewTransaction] = useState<Transaction | null>(
   //   null
   // );
-  const ctx = api.useContext();
+  // const ctx = api.useContext();
   console.log(session?.user.id);
 
   return (
     <Layout>
       {isConnected && session ? (
-        <>
+        <div className="-mt-20 flex h-screen flex-col items-center justify-center gap-20">
           <div>Welcome!</div>
+          <Send />
           <UserTransactions />
-        </>
+        </div>
       ) : (
         <div>Connect your wallet to get started</div>
       )}
--- src/server/api/routers/transactions.ts
+++ src/server/api/routers/transactions.ts
@@ -13,7 +13,31 @@ export const transactionsRouter = createTRPCRouter({
     return transactions;
   }),
   // //Add a new transaction
-  // add: protectedProcedure.input(z.object{
-
-  // })
+  add: protectedProcedure
+    .input(
+      z.object({
+        amount: z.number().min(0).max(1000000),
+        token: z.string().min(1).max(5),
+        amountInUSD: z.number().min(0).max(1000000),
+        phone: z.string().min(10).max(10).optional(),
+        txId: z.string(),
+        recipient: z.string().min(1).max(100).optional(),
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const userId = ctx.session.user.id;
+      const transaction = await ctx.prisma.transaction.create({
+        data: {
+          amount: input.amount,
+          token: input.token,
+          amountInUSD: input.amountInUSD,
+          phone: input.phone,
+          userId: userId,
+          txId: input.txId,
+          recipient: input.recipient,
+        },
+      });
+      return transaction;
+    }),
+  //When working with contacts figure out a way to save recent recipients and optionally add them as contacts
 });
