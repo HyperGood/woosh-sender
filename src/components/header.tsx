4195aaa1ac0c1d5923d388dcb92527bcfa13b1c6
roy-sandoval <roy@roysandoval.com>
2023-07-15 18:08:24 -0700

    Weird error on github fix

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,11 +1,7 @@
 import Logo from "public/images/Logo";
 import { AccountButton } from "./AccountButton";
 
-export default function Header({
-  noAccountButton,
-}: {
-  noAccountButton?: boolean;
-}) {
+export function Header({ noAccountButton }: { noAccountButton?: boolean }) {
   return (
     <div className="z-50 flex w-full items-center justify-between gap-4 px-4 py-4 lg:absolute lg:top-0 lg:px-10 lg:py-8">
       <Logo />
@@ -13,3 +9,5 @@ export default function Header({
     </div>
   );
 }
+
+export default Header;
