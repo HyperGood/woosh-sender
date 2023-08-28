0244f2887035355e7e4dcb7fdc8ef438ddb309de
roy-sandoval <roy@roysandoval.com>
2023-08-28 13:36:11 -0700

    Update pnpm version

--- src/lib/DepositVaultABI.ts
+++ src/lib/DepositVaultABI.ts
@@ -3,6 +3,9 @@ export const contractAddress = {
   31337: ["0x5FbDB2315678afecb367f032d93F642f64180aa3"],
 } as const;
 
+//updated contract
+//0xED082e987588125BEA1cBAe6F9380f37eC15D1B3 = 420
+
 export type Addresses = typeof contractAddress;
 
 export const abi = [
