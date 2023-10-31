ff6e2be11aa8f30c4c76b8b3d39807d429baa2a3
roy-sandoval <roy@roysandoval.com>
2023-10-30 23:35:25 -0700

    Add test error

--- src/components/SignIn.tsx
+++ src/components/SignIn.tsx
@@ -1,9 +1,13 @@
 import Logo from "public/images/Logo";
 
-import CustomConnectButton from "./CustomConnectButton";
+//import CustomConnectButton from "./CustomConnectButton";
 import PasskeySignIn from "./PasskeySignIn";
+import Button from "./Button";
 
 export const SignIn = () => {
+  function throwing() {
+    throw new Error("Sentry Frontend Error");
+  }
   return (
     <div className="flex h-screen w-full flex-col justify-between px-2 py-8 lg:px-0">
       <Logo />
@@ -14,6 +18,7 @@ export const SignIn = () => {
         <div className="flex w-full flex-col gap-4 lg:w-1/2">
           <PasskeySignIn />
           {/* <CustomConnectButton /> */}
+          <Button onClick={() => throwing()}>⚾️ Throw An Error</Button>
         </div>
       </div>
       <div />
